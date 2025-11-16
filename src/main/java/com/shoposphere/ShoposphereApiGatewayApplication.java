bb04ff264d582396e06612a54349c3a31f9c3d83
Anupam Anand <67553013+anupam0302@users.noreply.github.com>
2025-11-16 20:02:19 +0530

    Gateway chanegs

---
+++ src/main/java/com/shoposphere/ShoposphereApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.shoposphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoposphereApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoposphereApiGatewayApplication.class, args);
+	}
+
+}
